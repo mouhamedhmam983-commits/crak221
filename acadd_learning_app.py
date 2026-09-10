5f777a7ed5a4edbefce006c4c7adad8f6b6568b1
mouhamedhmam983-commits <mouhamedhmam983@gmail.com>
2026-09-10 20:44:19 +0000

    Fix sitemap indentation for verification route

--- acadd_learning_app.py
+++ acadd_learning_app.py
@@ -553,8 +553,8 @@ def sitemap():
     
     
 
-return Response(
-        f'<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{urls}</urlset>',
+    return Response(
+            f'<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{urls}</urlset>',
         mimetype='application/xml',
     )
 
